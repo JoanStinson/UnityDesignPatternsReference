1662c5d9c02c7075e003d213b1b1a128b79a4fba
Joan Ginard Mateo <jgm7.hd@gmail.com>
2022-02-23 15:53:05 +0100

    Singleton Pattern

---
+++ Retro RPG Patterns/Assets/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace JGM.Game
+{
+    public class GameManager : Singleton<GameManager>
+    {
+        private DateTime _sessionStartTime;
+        private DateTime _sessionEndTime;
+
+        private void Start()
+        {
+            // TODO:
+            // - Load player save
+            // - If no save, redirect player to registration scene
+            // - Call backend and get daily challenge and rewards 
+
+            _sessionStartTime = DateTime.Now;
+            Debug.Log("Game session start @: " + DateTime.Now);
+        }
+
+        private void OnApplicationQuit()
+        {
+            _sessionEndTime = DateTime.Now;
+            TimeSpan timeDifference = _sessionEndTime.Subtract(_sessionStartTime);
+
+            Debug.Log("Game session ended @: " + DateTime.Now);
+            Debug.Log("Game session lasted: " + timeDifference);
+        }
+
+        private void OnGUI()
+        {
+            if (GUILayout.Button("Next Scene"))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
+        }
+    }
+}
